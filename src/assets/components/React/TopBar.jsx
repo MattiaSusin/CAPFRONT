7525d841743e7675645044372822e734a0e342de
MattiaSusin <m.susin28@gmail.com>
2024-10-07 12:18:20 +0200

    TopBar

---
+++ src/assets/components/React/TopBar.jsx
@@ -0,0 +1,37 @@
+import {Container} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
+const TopBar = () => {
+    return(
+      
+      <Container fluid className="contTotTopBar pt-1">
+        <div className="LogoTopBar">
+          <NavLink to="/" className="nav-link">
+          <img src="../../../src/assets/Images/Logo_Footer.png" alt="" className="imgLogoTopBar"/>
+          </NavLink>
+        </div>
+        <div className="contSxTopBar">
+           <NavLink to="/" className="nav-link">
+          <p className="me-4 textTopBar pt-3">
+           HOME</p>
+           </NavLink>
+          <p className="me-4 textTopBar pt-3">CUCINA</p>
+          <NavLink to="/loungeBar" className="nav-link">
+          <p className="me-4 textTopBar pt-3">LOUNGE BAR</p>
+          </NavLink>
+          <NavLink to="/menu" className="nav-link">
+          <p className="me-4 textTopBar pt-3">MENU</p>
+          </NavLink>
+          <NavLink to="/contatti" className="nav-link">
+          <p className="me-4 textTopBar pt-3">CONTATTI</p>
+          </NavLink>
+          <NavLink to="/prenotazione" className="nav-link">
+          <button className="btnTopBar">PRENOTA</button>
+          </NavLink>
+        </div>
+      </Container>
+   
+    )
+}
+
+export default TopBar
---
+++ src/assets/components/React/TopBarMobile.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import {Container} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
+const TopBarMobile = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    console.log("menuOpen", menuOpen)
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen)
+      console.log("menuOpen", menuOpen)
+    };
+
+    return(
+      <Container fluid className="topBarMobile d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          <button className="btnTopBar" onClick={toggleMenu}>
+            {menuOpen ? <FaTimes/> : <FaBars/>}
+            </button>
+        </div>
+        {menuOpen && (
+          <div className="d-flex flex-column align-items-start mt-3">
+            <NavLink to="/" className="nav-link">
+              <p className="me-4 textTopBar pt-3">
+            HOME</p>
+            </NavLink>
+            <p className="me-4 textTopBar pt-3">CUCINA</p>
+            <NavLink to="/loungeBar" className="nav-link">
+            <p className="me-4 textTopBar pt-3">LOUNGE BAR</p>
+            </NavLink>
+            <NavLink to="/menu" className="nav-link">
+            <p className="me-4 textTopBar pt-3">MENU</p>
+            </NavLink>
+            <NavLink to="/contatti" className="nav-link">
+            <p className="me-4 textTopBar pt-3">CONTATTI</p>
+            </NavLink>
+            <NavLink to="/prenotazione" className="nav-link">
+            <button className="btnTopBar">PRENOTA</button>
+            </NavLink>
+          </div>   
+        )}
+      </Container>
+    )
+}
+
+export default TopBarMobile
