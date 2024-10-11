c7cae135f26770795f89619e8a39e27371ca86de
MattiaSusin <m.susin28@gmail.com>
2024-10-11 12:21:46 +0200

    Update MenuSettings.jsx

--- src/assets/components/React/MenuSettings.jsx
+++ src/assets/components/React/MenuSettings.jsx
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Css/MenuSettings.css";
 import {
+    Alert,
   Button,
   Form,
   FormControl,
-  FormGroup,
+
   FormLabel,
   Modal,
 } from "react-bootstrap";
@@ -16,6 +17,47 @@ const MenuSettings = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [formData, setFormData] = useState({
+    titolo: "",
+    descrizione: "",
+    prezzo: "",
+    image: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFileChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validate = () => {
+    let errors = {};
+    if (!formData.titolo) errors.titolo = "Il titolo è da inserire";
+    if (!formData.descrizione) errors.descrizione = "Il descrizione è da inserire";
+    if (!formData.prezzo) errors.prezzo = "L'prezzo è da inserire";
+    if (!formData.image) errors.image = "Il image è da inserire";
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validateErrors = validate();
+    if (Object.keys(validateErrors).length === 0) {
+      console.log("form submitted", formData);
+      setSuccess(true);
+    } else {
+      setErrors(validateErrors);
+      setSuccess(false);
+    }
+  };
+
   return (
     <div className="contMenuSettings mt-5  d-flex justify-content-center">
       <div>
@@ -24,11 +66,21 @@ const MenuSettings = () => {
         </div>
 
         <div className="mt-5 justify-content-center d-flex">
+        {success && (
+                      <>
+                        <Alert variant={"success"}>
+                          Form submitted successfully
+                        </Alert>
+                        <p>
+                          Riceverai una risposta quanto prima dal nostro staff
+                        </p>
+                      </>
+                    )}
           <Button variant="primary" onClick={handleShow}>
             NUOVO PIATTO
           </Button>
 
-          <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal show={show} onHide={handleClose} animation={false} onSubmit={handleSubmit}>
             <Modal.Header closeButton className="justify-content-center">
               <Modal.Title>NUOVO PIATTO</Modal.Title>
             </Modal.Header>
@@ -49,28 +101,28 @@ const MenuSettings = () => {
               <FormLabel className="mt-4">Titolo piatto*</FormLabel>
               <FormControl
                 type="text"
-                name="nome"
-                /* value={formData.nome}
+                name="titolo"
+                value={formData.titolo}
                           onChange={handleChange}
-                          isInvalid={!!errors.nome} */
+                          isInvalid={!!errors.titolo}
                 className=""
               />
               <FormControl.Feedback type="invalid">
-                {/* {errors.nome} */}
+                {/* {errors.titolo} */}
               </FormControl.Feedback>
 
               {/* --------------DESCRIZIONE---------------- */}
               <FormLabel className="mt-4">Descrizione piatto*</FormLabel>
               <FormControl
                 type="text"
-                name="nome"
-                /* value={formData.nome}
+                name="descrizione"
+                value={formData.descrizione}
                           onChange={handleChange}
-                          isInvalid={!!errors.nome} */
+                          isInvalid={!!errors.descrizione}
                 className=""
               />
               <FormControl.Feedback type="invalid">
-                {/* {errors.nome} */}
+                {/* {errors.descrizione} */}
               </FormControl.Feedback>
 
               {/* --------------PREZZO---------------- */}
@@ -79,26 +131,26 @@ const MenuSettings = () => {
               <Form.Control
                 type="number"
                 placeholder="Inserisci il prezzo"
-                /* value={price}
-              onChange={handlePriceChange} */
+                value={name}
+                onChange={handleChange}
                 min="0"
-                step="0.01" // Permette valori decimali
+                step="0.01" 
               />
 
 
             {/* --------------PREZZO---------------- */}
 
               <Form.Group controlId="formFile" className="mt-3">
-                <Form.Label>Seleziona un'immagine</Form.Label>
+                <Form.Label>Seleziona un&apos;immagine</Form.Label>
                 <Form.Control
                   type="file"
                   accept="image/*" 
-                  /* onChange={handleFileChange} */
+                  onChange={handleFileChange}
                 />
               </Form.Group>
             </Modal.Body>
             <Modal.Footer className="d-flex justify-content-center mt-3 mb-3 align-items-center">
-              <Button variant="primary" onClick={handleClose}>
+              <Button variant="primary" type="submit" onClick={handleClose}>
                 {" "}
                 GENERA PIATTO
               </Button>
